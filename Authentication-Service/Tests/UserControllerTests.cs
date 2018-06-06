a859c41aa941731046014d493f28170d2ed17512
Sebastian Skoczeń <skoczen.kontakt@gmail.com>
2018-06-06 23:22:53 +0200

    Unit Tests Added to Authentication Service

--- Authentication-Service/Tests/UserControllerTests.cs
+++ Authentication-Service/Tests/UserControllerTests.cs
@@ -30,7 +30,100 @@ namespace Authentication_Service.Tests
 
             // Assert
             Assert.AreNotEqual(null, response["id"]);
-            Assert.AreNotEqual(-1, response["id"]);
+            Assert.AreNotEqual(-1, (int)response["id"]);
+        }
+        [Test]
+        public void Register_UserDoesExist()
+        {
+            var controller = new UserController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            // Act
+            LiteDatabase db = new LiteDatabase(@"C:\database.db");
+            var col = db.GetCollection<User>();
+            if (!col.Exists(Query.EQ("username", "test"))) col.Insert(new User { username="test",password="test",status="user",id=0 });
+            var response = controller.Register(JObject.Parse("{\"username\": \"test\", \"password\": \"test\", \"status\": \"user\"}"));
+
+            // Assert
+            Assert.AreNotEqual(null, response["id"]);
+            Assert.AreEqual(-1, (int)response["id"]);
+        }
+        [Test]
+        public void Register_UsernameEmpty()
+        {
+            var controller = new UserController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            // Act
+            var response = controller.Register(JObject.Parse("{\"username\": \"\", \"password\": \"test\", \"status\": \"user\"}"));
+
+            // Assert
+            Assert.AreNotEqual(null, response["id"]);
+            Assert.AreEqual(-1, (int)response["id"]);
+        }
+        [Test]
+        public void Register_PasswordEmpty()
+        {
+            var controller = new UserController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            // Act
+            var response = controller.Register(JObject.Parse("{\"username\": \"test\", \"password\": \"\", \"status\": \"user\"}"));
+
+            // Assert
+            Assert.AreNotEqual(null, response["id"]);
+            Assert.AreEqual(-1, (int)response["id"]);
+        }
+        [Test]
+        public void Register_StatusEmpty()
+        {
+            var controller = new UserController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            // Act
+            var response = controller.Register(JObject.Parse("{\"username\": \"test\", \"password\": \"\", \"status\": \"\"}"));
+
+            // Assert
+            Assert.AreNotEqual(null, response["id"]);
+            Assert.AreEqual(-1, (int)response["id"]);
+        }
+        [Test]
+        public void Login_UserExists()
+        {
+            var controller = new UserController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            // Act
+            LiteDatabase db = new LiteDatabase(@"C:\database.db");
+            var col = db.GetCollection<User>();
+            if (!col.Exists(Query.EQ("username", "test"))) col.Insert(new User { username = "test", password = "test", status = "user", id = 0 });
+            var response = controller.Login(JObject.Parse("{\"username\": \"test\", \"password\": \"test\"}"));
+
+            // Assert
+            Assert.AreNotEqual(null, response["id"]);
+            Assert.AreNotEqual(-1, (int)response["id"]);
+        }
+        [Test]
+        public void Login_UserDoesNotExist()
+        {
+            var controller = new UserController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            // Act
+            LiteDatabase db = new LiteDatabase(@"C:\database.db");
+            var col = db.GetCollection<User>();
+            col.Delete(Query.EQ("username", "test"));
+            var response = controller.Login(JObject.Parse("{\"username\": \"test\", \"password\": \"test\"}"));
+
+            // Assert
+            Assert.AreNotEqual(null, response["id"]);
+            Assert.AreEqual(-1, (int)response["id"]);
         }
     }
 }
