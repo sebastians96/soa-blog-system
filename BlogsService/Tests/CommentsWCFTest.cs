d1c08ce53a489eaac99cfa0071702169aa258ca8
sienmich <michal.sienczak@gmail.com>
2018-06-10 22:19:34 +0200

    TestAdapter package fix

---
+++ BlogsService/Tests/CommentsWCFTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogsService.Tests
+{
+    [TestFixture]
+    public class CommentsWCFTest
+    {
+        private BlogService _blogService;
+
+        public CommentsWCFTest()
+        {
+            this._blogService = new BlogService();
+        }
+
+        [Test]
+        public void GetAllCommentsTest()
+        {
+            var response = _blogService.GetAllComments();
+            Assert.NotNull(response);
+        }
+
+        [Test]
+        public void GetCommentByIDTest()
+        {
+            var response = _blogService.GetCommentById(2);
+
+        }
+
+        [Test]
+        public void PostCommentTest()
+        {
+
+        }
+
+        [Test]
+        public void DeleteCommentTest()
+        {
+
+        }
+    }
+}
